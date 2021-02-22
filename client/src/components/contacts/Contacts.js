f9d02b9e65997cfc8b0fe9b385b10174a7033e6e
jmusyoki <jmusyoki@wastehero.io>
2021-02-22 12:06:06 +0100

    Created form CRUD on contacts & add-delete animation

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,18 +1,32 @@
 import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from '../contacts/ContactItem';
 import ContactContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts } = contactContext;
-  console.log(contacts);
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    <h4>Please add a contact</h4>;
+  }
 
   return (
     <Fragment>
-      {contacts.map((contact) => (
-        <ContactItem key={contact.id} contact={contact}></ContactItem>
-      ))}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact}></ContactItem>
+              </CSSTransition>
+            ))
+          : contacts.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem className='' contact={contact}></ContactItem>
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </Fragment>
   );
 };
